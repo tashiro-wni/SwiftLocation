4bbe195e2a389842038722b04716d34382b3ce0c
Tomohiro Tashiro <tomohiro.tashiro@gmail.com>
2019-02-16 17:01:04 +0900

    update for Xcode 10

--- SwiftLocation/ViewController.swift
+++ SwiftLocation/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
-    @IBOutlet var mapView :MKMapView
+    @IBOutlet weak var mapView: MKMapView!
     
-    var locationManager: CLLocationManager = CLLocationManager()
+    private let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,73 +23,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = 500
-        
-        if( locationManager.respondsToSelector(Selector("requestAlwaysAuthorization")) ){
-            //locationManager.requestWhenInUseAuthorization()
-            locationManager.requestAlwaysAuthorization()
-        } else {
-            locationManager.startUpdatingLocation()
-            println("startUpdatingLocation")
-        }
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+}
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    
-// CLLocationManagerDelegate
-    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        var str: String
-        switch(status){
-            case .NotDetermined:       str = "NotDetermined"
-            case .Restricted:          str = "Restricted"
-            case .Denied:              str = "Denied"
-            case .Authorized:          str = "Authorized"
-            case .AuthorizedWhenInUse: str = "AuthorizedWhenInUse"
-        }
-        println("locationManager auth status changed, \(str)")
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+
+        locationManager.stopUpdatingLocation()
+        mapView.setCenter(location.coordinate, animated: true)
+
+        // reverse geocoding
+        CLGeocoder().reverseGeocodeLocation(location, completionHandler: { [weak self] (placemarks, error) in
+            guard let placemarks = placemarks, let placemark = placemarks.first else { return }
             
-        if( status == .Authorized || status == .AuthorizedWhenInUse ) {
-            locationManager.startUpdatingLocation()
-            println("startUpdatingLocation")
-        }
-    }
-    
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        println("found \(locations.count) placemarks.")
-        if( locations.count > 0 ){
-            let location = locations[0] as CLLocation;
-            let region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(0.01,0.01))
-            self.mapView.setRegion(region, animated:true)  // Zoom to Current Location.
-        
-            locationManager.stopUpdatingLocation()
-            println("location updated, lat:\(location.coordinate.latitude), lon:\(location.coordinate.longitude), stop updating.")
-        
-            // reverse geocoding
-            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { placemarks, error in
-                println("found \(placemarks.count) placemarks.")
-                if( placemarks.count > 0 ) {
-                    let placemark = placemarks[0] as CLPlacemark
-                    println("placemark:\(placemark.locality), \(placemark.subLocality)")  // ex. Shibuya, Dogenzaka
+            let text = placemark.locality ?? ""
             
-                    let point = MKPointAnnotation()
-                    point.coordinate = location.coordinate
-                    point.title = "I'm here"
-                    point.subtitle = "\(placemark.locality), \(placemark.subLocality)"
-                    self.mapView.addAnnotation(point)
-                }
-            })
-        }
+            let point = MKPointAnnotation()
+            point.coordinate = location.coordinate
+            point.title = "I'm here"
+            point.subtitle = text
+            self?.mapView.addAnnotation(point)
+        })
     }
 
-    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
-        println("location didFailWithError, \(error)")
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("\(error.localizedDescription)")
     }
-
 }
 
